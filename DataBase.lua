bc0cd883becb58f4894a7c4e7ccfe9d5281d2057
xTkxl5 <113262915+xTkxl5@users.noreply.github.com>
2022-09-18 19:54:48 -0500

    Update DataBase.lua

--- DataBase.lua
+++ DataBase.lua
@@ -1,15 +1,1 @@
-DataBase = "Mango Hub vPremium | Data Base Loaded"
-print(DataBase)
-local vu = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-    wait(1)
-    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
 
-spawn(function()
-    game.RunService.RenderStepped:Connect(function()
-        game.Players.LocalPlayer.MaximumSimulationRadius = 1000;
-        setsimulationradius(1000,1000)
-    end)
-end)
