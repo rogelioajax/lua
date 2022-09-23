685d3d2e75a2115c3f432b0b0a3cb5b751c7887d
Rogelio Ajax <110694111+rogelioajax@users.noreply.github.com>
2022-09-22 21:22:20 -0500

    Added Functions Secrets XD

--- DataBase.lua
+++ DataBase.lua
@@ -1,15 +1,1 @@
-print("▬▬ι═══════ﺤ")
-print("▬▬ι═══════ﺤ")
-print("ඞ ඞ ඞsusඞ ඞ ඞ")
-if not writefile or not readfile then 
-    pcall(function()
-        game.StarterGui:SetCore("SendNotification", {
-          Title = 'Warning!';
-          Text = 'Your executor does not support Mango Premium. Use executor level 7 or 8.';
-          Duration = 100;
-          Icon = "rbxassetid://6525527344";
-        })
-      end)
-    return else 
-            print("Executor Supported :D") 
-    end
+lol
