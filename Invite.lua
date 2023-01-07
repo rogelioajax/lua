8646d447254273d492a6dd722f66ea60356d4614
Pop Smoke <110694111+rogelioajax@users.noreply.github.com>
2023-01-06 23:50:44 -0600

    Update Invite.lua

--- Invite.lua
+++ Invite.lua
@@ -1,1 +1,53 @@
-
+coroutine.resume(coroutine.create(function()
+    wait()
+    game.StarterGui:SetCore("SendNotification", {
+        Title = 'DISCORD';
+        Text = 'Discord Server Copied to your clipboard!';
+        Duration = 10;
+    })
+    setclipboard('https://discord.gg/vzX5qh8MhA')
+end))
+local Settings = {
+            InviteCode = "vzX5qh8MhA"
+        }
+ 
+        local HttpService = game:GetService("HttpService")
+        local RequestFunction
+ 
+        if syn and syn.request then
+            RequestFunction = syn.request
+        elseif request then
+            RequestFunction = request
+        elseif http and http.request then
+            RequestFunction = http.request
+        elseif http_request then
+            RequestFunction = http_request
+        end
+ 
+        local DiscordApiUrl = "http://127.0.0.1:%s/rpc?v=1"
+ 
+        if not RequestFunction then
+            return print("Your executor does not support http requests.")
+        end
+ 
+        for i = 6453, 6464 do
+            local DiscordInviteRequest = function()
+                local Request = RequestFunction({
+                    Url = string.format(DiscordApiUrl, tostring(i)),
+                    Method = "POST",
+                    Body = HttpService:JSONEncode({
+                        nonce = HttpService:GenerateGUID(false),
+                        args = {
+                            invite = {code = Settings.InviteCode},
+                            code = Settings.InviteCode
+                        },
+                        cmd = "INVITE_BROWSER"
+                    }),
+                    Headers = {
+                        ["Origin"] = "https://discord.com",
+                        ["Content-Type"] = "application/json"
+                    }
+                })
+            end
+            spawn(DiscordInviteRequest)
+        end
