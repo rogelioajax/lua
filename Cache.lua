e6f53f06296d2ad1d5b082179e5451e586542ab6
rogelioajax <110694111+rogelioajax@users.noreply.github.com>
2025-03-22 19:48:40 -0600

    Update Cache.lua

--- Cache.lua
+++ Cache.lua
@@ -1,11 +1,8 @@
 return {
-    date = "2025-03-22",
     version = "2.8.9",
-    exploit = "identifyexecutor()",
     ranks = {
-        ["8171996710"] = "Owner", -- A nigger
-        ["987654321"] = "Admin", --Idk
+        ["8171996710"] = "[🔥] Developer", -- A nigger lol
+        ["987654321"] = "Admin",
         ["112233445"] = "Moderator"
     }
 }
---safsaf
