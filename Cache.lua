93096e6be2565d9e2ce4d1c987af92cb3657dbbb
rogelioajax <110694111+rogelioajax@users.noreply.github.com>
2025-04-04 19:24:35 -0600

    Update Cache.lua

--- Cache.lua
+++ Cache.lua
@@ -1,8 +1,6 @@
 return {
     version = "2.8.9",
     ranks = {
-        ["3772307869"] = "[🔥] Developer", -- A nigger lol 
-        ["987654321"] = "Admin",
-        ["112233445"] = "Moderator"
+        ["3772307869"] = "[🔥] Developer"
     }
 }
